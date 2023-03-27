1d82d91bef0b77f9cf069bf07977460b018dd971
Muhammad_Hammad_Shah <hammad4455shah5544@gmail.com>
2023-03-28 03:31:25 +0500

    home modified

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -1,18 +1,30 @@
+import Image from "next/image";
 import React from "react";
 
 export const Home = () => {
   return (
-    <div>
-      <h1>I am a Full Stack Developer</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis
-        architecto expedita esse dolor tenetur dolorum illum earum porro nisi,
-        facere quis voluptatum illo perferendis, consequatur odit velit
-        veritatis dolore ea necessitatibus eligendi labore itaque unde aut
-        sequi. Eum facilis excepturi beatae molestiae soluta, aperiam, atque
-        accusamus sed adipisci sequi quam!
-      </p>
-      <button></button>
-    </div>
+    <section>
+      {/* left corner starts */}
+      <div className="p-5">
+        <h1>I am a Full Stack Developer</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis
+          architecto expedita esse dolor tenetur dolorum illum earum porro nisi,
+          facere quis voluptatum illo perferendis, consequatur odit velit
+          veritatis dolore ea necessitatibus eligendi labore itaque unde aut
+          sequi. Eum facilis excepturi beatae molestiae soluta, aperiam, atque
+          accusamus sed adipisci sequi quam!
+        </p>
+        <button className="bg-gradient-to-r from-cyan-500 to-blue-500 px-3 py-2 rounded-lg">
+          Portfolio
+        </button>
+      </div>
+      {/* left corner ends */}
+      <div className="p-5">
+        {/* right corner starts */}
+        <Image />
+        {/* right corner ends */}
+      </div>
+    </section>
   );
 };
