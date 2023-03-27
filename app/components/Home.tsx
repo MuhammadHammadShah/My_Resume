c2ac2c6e3e3f74dd7da2ed5048fac07110b90689
Muhammad_Hammad_Shah <hammad4455shah5544@gmail.com>
2023-03-28 00:18:00 +0500

    changing and styling again but this time in globalcss styling for h1 is permanent

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -1,5 +1,18 @@
 import React from "react";
 
 export const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <h1>I am a Full Stack Developer</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis
+        architecto expedita esse dolor tenetur dolorum illum earum porro nisi,
+        facere quis voluptatum illo perferendis, consequatur odit velit
+        veritatis dolore ea necessitatibus eligendi labore itaque unde aut
+        sequi. Eum facilis excepturi beatae molestiae soluta, aperiam, atque
+        accusamus sed adipisci sequi quam!
+      </p>
+      <button></button>
+    </div>
+  );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import { Home } from "./components/Home";
 import { Header } from "./components/Header";
 
-export const page = () => {
+
+
+export default function page() {
   return (
     <div>
       <Header />
       <Home />
     </div>
   );
-};
+}
