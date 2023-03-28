2d5c54cbc2b959b725f5196137a38c9fc8d95a58
Muhammad_Hammad_Shah <hammad4455shah5544@gmail.com>
2023-03-28 19:50:41 +0500

    made some changes and installed react-icons and used it with the button profile and an image is also added

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -9,11 +9,21 @@ export const Header = () => {
             Muhammad Hammad Shah
           </h1>
           <ul className="flex m-7">
-            <li className="text-2xl mx-5 font-khanBaBa">Home</li>
-            <li className="text-2xl mx-5 font-khanBaBa">About</li>
-            <li className="text-2xl mx-5 font-khanBaBa">Skill</li>
-            <li className="text-2xl mx-5 font-khanBaBa">Portfolio</li>
-            <li className="text-2xl mx-5 font-khanBaBa">Contact</li>
+            <li className="text-2xl mx-5 font-khanBaBa hover:scale-150 duration-200">
+              Home
+            </li>
+            <li className="text-2xl mx-5 font-khanBaBa hover:scale-150 duration-200">
+              About
+            </li>
+            <li className="text-2xl mx-5 font-khanBaBa hover:scale-150 duration-200">
+              Skill
+            </li>
+            <li className="text-2xl mx-5 font-khanBaBa hover:scale-150 duration-200">
+              Portfolio
+            </li>
+            <li className="text-2xl mx-5 font-khanBaBa hover:scale-150 duration-200">
+              Contact
+            </li>
           </ul>
         </div>
       </section>
--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -1,9 +1,9 @@
 import Image from "next/image";
 import React from "react";
-
+import { MdKeyboardArrowRight } from "react-icons/md";
 export const Home = () => {
   return (
-    <section>
+    <section className=" max-w-4xl mx-auto flex md:flex-row sm:flex-col">
       {/* left corner starts */}
       <div className="p-5">
         <h1>I am a Full Stack Developer</h1>
@@ -15,14 +15,21 @@ export const Home = () => {
           sequi. Eum facilis excepturi beatae molestiae soluta, aperiam, atque
           accusamus sed adipisci sequi quam!
         </p>
-        <button className="bg-gradient-to-r from-cyan-500 to-blue-500 px-3 py-2 rounded-lg">
-          Portfolio
+        <button className=" group flex justify-center items-center  bg-gradient-to-r from-cyan-500 to-blue-500 px-3 py-2 rounded-lg">
+          Profile
+          <MdKeyboardArrowRight
+            size={20}
+            className="group-hover:rotate-90 duration-500"
+          />
         </button>
       </div>
       {/* left corner ends */}
       <div className="p-5">
         {/* right corner starts */}
-        <Image />
+        <img
+          src="https://cdn.pixabay.com/photo/2016/03/12/23/18/man-1253004__340.jpg"
+          alt="just an image"
+        />
         {/* right corner ends */}
       </div>
     </section>
