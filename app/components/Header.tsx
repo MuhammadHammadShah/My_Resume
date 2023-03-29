fbb30e100e1d1cf1fc833bb97db90fd8a182a96b
Muhammad_Hammad_Shah <hammad4455shah5544@gmail.com>
2023-03-29 20:30:28 +0500

    finishing the hamburger button

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,6 +1,9 @@
-import React from "react";
-
+"use client";
+import React, { useState } from "react";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { RxCross2 } from "react-icons/rx";
 export const Header = () => {
+  const [toggle, setToggle] = useState(false);
   return (
     <div>
       <section className=" bg-gray-700 text-white">
@@ -8,7 +11,35 @@ export const Header = () => {
           <h1 className="text-3xl font-bold font-signature m-7 ">
             Muhammad Hammad Shah
           </h1>
-          <ul className="flex m-7">
+          <ul className="m-7 md:flex hidden ">
+            <li className="text-2xl mx-5 font-khanBaBa hover:scale-150 duration-200">
+              Home
+            </li>
+            <li className="text-2xl mx-5 font-khanBaBa hover:scale-150 duration-200">
+              About
+            </li>
+            <li className="text-2xl mx-5 font-khanBaBa hover:scale-150 duration-200">
+              Skill
+            </li>
+            <li className="text-2xl mx-5 font-khanBaBa hover:scale-150 duration-200">
+              Portfolio
+            </li>
+            <li className="text-2xl mx-5 font-khanBaBa hover:scale-150 duration-200">
+              Contact
+            </li>
+          </ul>
+          <div
+            className="pr-5 md:hidden z-10"
+            onClick={() => {
+              setToggle(!toggle);
+            }}
+          >
+            {toggle ? <GiHamburgerMenu size={30} /> : <RxCross2 size={30} />}
+          </div>
+        </div>
+        {/* overlay stays here  */}
+        <div className="fixed top-0 left-0 bg-slate-600 h-screen text-red-500 w-full flex justify-center items-center  ">
+          <ul className="md:flex left-10 text-white m-7 ">
             <li className="text-2xl mx-5 font-khanBaBa hover:scale-150 duration-200">
               Home
             </li>
@@ -26,6 +57,7 @@ export const Header = () => {
             </li>
           </ul>
         </div>
+        {/* overlay ends here  */}
       </section>
     </div>
   );
