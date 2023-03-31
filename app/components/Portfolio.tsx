49d3ee9b15cd9e832f6d21ee6116fe5aa89e7a60
Muhammad_Hammad_Shah <hammad4455shah5544@gmail.com>
2023-03-31 22:14:18 +0500

    .

--- app/components/Portfolio.tsx
+++ app/components/Portfolio.tsx
@@ -3,33 +3,145 @@ import React from "react";
 export const Portfolio = () => {
   return (
     <div
-      className="flex items-center max-w-7xl mx-auto leading-8 mb-10"
+      className=" items-center max-w-7xl mx-auto leading-8 mb-10"
       id="Portfolio"
     >
-      <div>
-        <h1 className="text-center text-4xl text-white mb-10">Portfolio</h1>;
+      <div className=" text-gray-500 px-10 mt-32 mx-auto">
+        <h1 className="text-center text-4xl text-white mb-10">Portfolio</h1>
       </div>
-      {/* Card_1 starts from here  */}
-      <div className=" shadow-md shadow-gray-400  h-60 hover:scale-105 duration-75">
-        <div>
-          <img
-            src="https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aHVtYW58ZW58MHx8MHx8&w=1000&q=80"
-            alt="any_image"
-            className=" w-60 h-40 rounded-t-md"
-          />
-        </div>
+      <div className=" grid sm:grid-cols-2 md:grid-cols-3 gap-20 mx-auto w-full">
+        {/* Card_1 starts from here  */}
+        <div className=" shadow-md shadow-gray-400  h-40 hover:scale-105 duration-75">
+          <div>
+            <img
+              src="https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aHVtYW58ZW58MHx8MHx8&w=1000&q=80"
+              alt="any_image"
+              className=" w-full h-40 rounded-t-md"
+            />
+          </div>
 
-        <div className="flex justify-between px-5 text-white">
-          <button className=" p-4 hover:scale-105 duration-200">
-            <a href="#">Demo</a>
-          </button>
+          <div className="flex justify-between px-5 text-white">
+            <button className=" p-4 hover:scale-105 duration-200">
+              <a href="#">Demo</a>
+            </button>
 
-          <button className=" p-4 hover:scale-105 duration-200">
-            <a href="#">Code</a>
-          </button>
+            <button className=" p-4 hover:scale-105 duration-200">
+              <a href="#">Code</a>
+            </button>
+          </div>
         </div>
+        {/* Card_1 ends from here  */}
+
+        {/* Card_2 starts from here */}
+        <div className=" shadow-md shadow-gray-400  h-60 hover:scale-105 duration-75">
+          <div>
+            <img
+              src="https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aHVtYW58ZW58MHx8MHx8&w=1000&q=80"
+              alt="any_image"
+              className=" w-60 h-40 rounded-t-md"
+            />
+          </div>
+
+          <div className="flex justify-between px-5 text-white">
+            <button className=" p-4 hover:scale-105 duration-200">
+              <a href="#">Demo</a>
+            </button>
+
+            <button className=" p-4 hover:scale-105 duration-200">
+              <a href="#">Code</a>
+            </button>
+          </div>
+        </div>
+        {/* Card_2 ends here here */}
+
+        {/* Card_3 starts from here */}
+        <div className=" shadow-md shadow-gray-400  h-60 hover:scale-105 duration-75">
+          <div>
+            <img
+              src="https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aHVtYW58ZW58MHx8MHx8&w=1000&q=80"
+              alt="any_image"
+              className=" w-60 h-40 rounded-t-md"
+            />
+          </div>
+
+          <div className="flex justify-between px-5 text-white">
+            <button className=" p-4 hover:scale-105 duration-200">
+              <a href="#">Demo</a>
+            </button>
+
+            <button className=" p-4 hover:scale-105 duration-200">
+              <a href="#">Code</a>
+            </button>
+          </div>
+        </div>
+        {/* Card_3 ends here here */}
+
+        {/* Card_4 starts from here */}
+        <div className=" shadow-md shadow-gray-400  h-60 hover:scale-105 duration-75">
+          <div>
+            <img
+              src="https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aHVtYW58ZW58MHx8MHx8&w=1000&q=80"
+              alt="any_image"
+              className=" w-60 h-40 rounded-t-md"
+            />
+          </div>
+
+          <div className="flex justify-between px-5 text-white">
+            <button className=" p-4 hover:scale-105 duration-200">
+              <a href="#">Demo</a>
+            </button>
+
+            <button className=" p-4 hover:scale-105 duration-200">
+              <a href="#">Code</a>
+            </button>
+          </div>
+        </div>
+        {/* Card_4 ends here here */}
+
+        {/* Card_5 starts from here */}
+        <div className=" shadow-md shadow-gray-400  h-60 hover:scale-105 duration-75">
+          <div>
+            <img
+              src="https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aHVtYW58ZW58MHx8MHx8&w=1000&q=80"
+              alt="any_image"
+              className=" w-60 h-40 rounded-t-md"
+            />
+          </div>
+
+          <div className="flex justify-between px-5 text-white">
+            <button className=" p-4 hover:scale-105 duration-200">
+              <a href="#">Demo</a>
+            </button>
+
+            <button className=" p-4 hover:scale-105 duration-200">
+              <a href="#">Code</a>
+            </button>
+          </div>
+        </div>
+        {/* Card_5 ends here here */}
+
+        {/* Card_6 starts from here */}
+        <div className=" shadow-md shadow-gray-400  h-60 hover:scale-105 duration-75">
+          <div>
+            <img
+              src="https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aHVtYW58ZW58MHx8MHx8&w=1000&q=80"
+              alt="any_image"
+              className=" w-60 h-40 rounded-t-md"
+            />
+          </div>
+
+          <div className="flex justify-between px-5 text-white">
+            <button className=" p-4 hover:scale-105 duration-200">
+              <a href="#">Demo</a>
+            </button>
+
+            <button className=" p-4 hover:scale-105 duration-200">
+              <a href="#">Code</a>
+            </button>
+          </div>
+        </div>
+        {/* Card_6 ends here here */}
       </div>
-      ;{/* Card_2 ends from here  */}
     </div>
   );
 };
